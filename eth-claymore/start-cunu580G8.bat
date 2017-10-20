07293ce44c3ee787b065c165f432fc0999e133a9
anhbinu <anhbinu@gmail.com>
2017-10-20 18:03:14 +0700

    udap

--- eth-claymore/start-cunu580G8.bat
+++ eth-claymore/start-cunu580G8.bat
@@ -3,4 +3,7 @@ setx GPU_MAX_HEAP_SIZE 100
 setx GPU_USE_SYNC_OBJECTS 1
 setx GPU_MAX_ALLOC_PERCENT 100
 setx GPU_SINGLE_ALLOC_PERCENT 100
+
+TIMEOUT /T 20
+
 EthDcrMiner64.exe -epool asia1.ethermine.org:4444 -ewal 0xf7afdc43a6c704c089cf9d1e48dea1600e78f251.cunu580G8
