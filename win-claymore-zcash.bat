2db67b1064c2d4fda01c7213e5dc6adf3602593a
anhbinu <anhbinu@gmail.com>
2017-11-19 21:49:35 +0700

    Update Miner

--- win-claymore-zcash.bat
+++ win-claymore-zcash.bat
@@ -6,5 +6,5 @@ setx GPU_USE_SYNC_OBJECTS 1
 setx GPU_MAX_ALLOC_PERCENT 100
 setx GPU_SINGLE_ALLOC_PERCENT 100
 
-zcash-claymore\ZecMiner64.exe -zpool asia1-zcash.flypool.org:3333 -zwal t1T8TM2AoyjcqJQJVaCviXca7chKQ3MoQEn.miner1 -zpsw x
+zcash-claymore-amd\ZecMiner64.exe -zpool asia1-zcash.flypool.org:3333 -zwal t1T8TM2AoyjcqJQJVaCviXca7chKQ3MoQEn.miner1 -zpsw x
 
