a50dfcec4cc9f55aa4c0338e655e194edc82ef5a
anhbinu <anhbinu@gmail.com>
2017-11-20 01:17:49 +0700

    fix bug

--- xmr-claymore-amd-window.bat
+++ xmr-claymore-amd-window.bat
@@ -5,4 +5,5 @@ GPU_MAX_HEAP_SIZE 100
 GPU_USE_SYNC_OBJECTS 1
 GPU_MAX_ALLOC_PERCENT 100
 GPU_SINGLE_ALLOC_PERCENT 100
-claymore-cryptonote-amd/NsGpuCNMiner.exe -zpool xmr-eu.suprnova.cc:5222 -zwal anhbinu.amd -zpsw 25251325 -allpools 1
+
+claymore-cryptonote-amd/NsGpuCNMiner.exe -xpool stratum+tcp://xmr-eu.suprnova.cc:5222 -xwal anhbinu.amd -xpsw 25251325 -allpools 1
