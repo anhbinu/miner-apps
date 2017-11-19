efe5c915a3b492f0b2ddaea2e9e8a47145de351e
anhbinu <anhbinu@gmail.com>
2017-11-20 01:06:45 +0700

    Add XMR

---
+++ xmr-claymore-amd-window.bat
@@ -0,0 +1,8 @@
+TIMEOUT /T 20
+
+GPU_FORCE_64BIT_PTR 1
+GPU_MAX_HEAP_SIZE 100
+GPU_USE_SYNC_OBJECTS 1
+GPU_MAX_ALLOC_PERCENT 100
+GPU_SINGLE_ALLOC_PERCENT 100
+btg-claymore-amd/NsGpuCNMiner.exe -zpool xmr-eu.suprnova.cc:5222 -zwal anhbinu.amd -zpsw 25251325 -allpools 1
