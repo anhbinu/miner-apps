ddba5a6e31614c7738d079458b63b982e648f235
anhbinu <anhbinu@gmail.com>
2017-11-21 13:45:10 +0700

    Update config

--- btg-claymore-amd-window.bat
+++ btg-claymore-amd-window.bat
@@ -6,4 +6,4 @@ GPU_USE_SYNC_OBJECTS 1
 GPU_MAX_ALLOC_PERCENT 100
 GPU_SINGLE_ALLOC_PERCENT 100
 
-btg-claymore-amd\ZecMiner64.exe -zpool asia.pool.gold:3044 -zwal GcWbn5NyNJVkLNnFJY5Hmhf5PraQbWQjKH.amd -zpsw 25251325 -allpools 1
+btg-claymore-amd\ZecMiner64.exe -zpool btg.suprnova.cc:8816 -zwal anhbinu.amd -zpsw 25251325 -allpools 1
